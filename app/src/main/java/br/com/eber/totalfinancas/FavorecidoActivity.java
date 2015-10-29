f777d1bc99664a76f0585fede716b5ac84ba08f5
eber-documentos <eber.documentos@gmail.com>
2015-10-28 23:47:08 -0200

    Initial commit

---
+++ app/src/main/java/br/com/eber/totalfinancas/FavorecidoActivity.java
@@ -0,0 +1,30 @@
+package br.com.eber.totalfinancas;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class FavorecidoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorecido);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle(getString(R.string.favorecido));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
