9564ad16c4365d6a38d4cfe030faf4462bd41e4b
eber-documentos <eber.documentos@gmail.com>
2015-11-04 22:08:24 -0200

    Remoção das Tabs

--- app/src/main/java/br/com/eber/totalfinancas/FavorecidoActivity.java
+++ app/src/main/java/br/com/eber/totalfinancas/FavorecidoActivity.java
@@ -27,12 +27,6 @@ public class FavorecidoActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
-
-        DesignDemoPagerAdapter adapter = new DesignDemoPagerAdapter(getSupportFragmentManager());
-        ViewPager viewPager = (ViewPager)findViewById(R.id.viewpager);
-        viewPager.setAdapter(adapter);
-        TabLayout tabLayout = (TabLayout)findViewById(R.id.tablayout);
-        tabLayout.setupWithViewPager(viewPager);
     }
 
     @Override
@@ -45,61 +39,5 @@ public class FavorecidoActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public static class DesignDemoFragment extends Fragment {
-        private static final String TAB_POSITION = "tab_position";
-
-        public DesignDemoFragment() {
-
-        }
-
-        public static DesignDemoFragment newInstance(int tabPosition) {
-            DesignDemoFragment fragment = new DesignDemoFragment();
-            Bundle args = new Bundle();
-            args.putInt(TAB_POSITION, tabPosition);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Nullable
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            Bundle args = getArguments();
-            int tabPosition = args.getInt(TAB_POSITION);
-            TextView tv = new TextView(getActivity());
-            tv.setGravity(Gravity.CENTER);
-            tv.setText("Text in Tab #" + tabPosition);
-            return tv;
-        }
-    }
-
-    static class DesignDemoPagerAdapter extends FragmentStatePagerAdapter {
-
-        public DesignDemoPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return DesignDemoFragment.newInstance(position);
-        }
-
-        @Override
-        public int getCount() {
-            return 2;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            String descricao;
-            if (position == 0) {
-                descricao = "Pesquisa";
-            } else {
-                descricao = "Manutenção";
-            }
-
-            return descricao;
-        }
     }
 }
